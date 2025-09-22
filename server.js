808d8984a55672d07440c67e0028c2b419c9dc95
Nashwa Arshad <Nashwaarshad123@gmail.com>
2025-09-22 12:10:09 +0530

    added

---
+++ server.js
@@ -0,0 +1,38 @@
+const express=require('express');
+const cors=require('cors');
+const fs=require('fs');
+const path=require('path');
+
+const app=express();
+app.use(corss());
+app.use(express.json());
+
+const filePath = path.join(__dirname,"data","recipe.json");
+
+app.get("/api/recipe.json",(req,res)=>{
+    fs.readFile(filePath,'utf8',(err,data)=>{
+        res.json(JSON.parse(data || "[]"));
+    })
+})
+
+app.post("/api/recipe.json",(req,res)=>{
+    if(!title||!ingredients||!instructions){
+        res.status(400);
+    }
+    const {title,ingredients,instructions,cookTime}=req.body;
+    const newRecipe={
+        id :Date.now(),
+        title,
+        ingredients,
+        instructions,
+        cookTime,
+        difficulty:"medium"
+    }
+fs.readFile(filePath,"utf8",(err,data)=>{
+    const recipe=JSON.parse(data ||"[]");
+    recipe.push(newRecipe);
+    fs.writeFile(filePath,JSON.stringify(recipe,null,2),()=>{
+        res.status(201).json(newRecipe);
+    })
+})
+})
